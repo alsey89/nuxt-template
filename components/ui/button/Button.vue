cb00f1433fdd3d0263f179122ae18805ad1fd77b
alsey89 <101123930+alsey89@users.noreply.github.com>
2024-03-02 00:32:13 +0800

    fix: fixed missing asChild in primitiveProps extension interface

--- components/ui/button/Button.vue
+++ components/ui/button/Button.vue
@@ -8,6 +8,7 @@ interface Props extends /* @vue-ignore */ PrimitiveProps {
   variant?: ButtonVariants['variant']
   size?: ButtonVariants['size']
   as?: string
+  asChild?: boolean
   class?: HTMLAttributes['class']
 }
 
@@ -17,6 +18,11 @@ const props = withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
+  <!-- `asChild` prop enables the `Primitive` component to render its content directly into its parent,
+  omitting an extra DOM wrapper. This is useful for maintaining semantic HTML and reducing unnecessary nesting,
+  by allowing the component to pass all props, slots, and event listeners to its child, thus avoiding additional element
+  layers.
+  Useful for flexible UI composition and ensuring clean, semantic HTML structures. -->
   <Primitive :as="as" :as-child="asChild" :class="cn(buttonVariants({ variant, size }), props.class)">
     <slot />
   </Primitive>
